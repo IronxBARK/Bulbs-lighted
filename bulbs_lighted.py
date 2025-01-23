65efcfd1af65ef3f11003ace213926ddfab53bca
IronBark <140833503+IronxBARK@users.noreply.github.com>
2025-01-23 14:55:52 +0530

    Update bulbs_lighted.py

--- bulbs_lighted.py
+++ bulbs_lighted.py
@@ -1,32 +1,26 @@
-import math
-
 def light_bulbs(bulbs, persons):
     on = []
     off = []
-    if bulbs == persons or persons > bulbs:
-        for num in range(1, bulbs+1):
-            sq = num**0.5
-            if sq.is_integer():
-                on.append(num)
-            else:
+    
+    for num in range(1, bulbs+1):
+            lst = []
+            for x in range(1, num+1):
+                if num%x == 0 and x<=persons:
+                    lst.append(x)
+            if (len(lst)) % 2 == 0:
                 off.append(num)
-             
-    else:
-        for num in range(1, bulbs+1):
-            sq = num**0.5
-            if sq.is_integer():
-                if num <= persons:
-                    on.append(num)
-                else:
-                    off.append(num)
             else:
-                if num <= persons:
-                    off.append(num)
-                else:
-                    on.append(num)
-   
+                on.append(num)
+                
     print(f"Bulbs on are: {len(on)}")
     print(f"Bulbs off are: {len(off)}")   
+
+light_bulbs(1000,100)
+          
+            
+
+
+
           
             
 light_bulbs(1000,1000)
